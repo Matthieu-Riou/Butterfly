6a7d1d46a378a124090d42ccd85a351de3fc79f7
Matthieu-Riou <matthieu.riou@etu.univ-nantes.fr>
2014-11-11 14:51:52 +0100

    First step, refactoring the historic

--- src/main/scala/io/atal/butterfly/Buffer.scala
+++ src/main/scala/io/atal/butterfly/Buffer.scala
@@ -19,23 +19,35 @@ class Buffer(s: String) {
     */
   def select(beginIndex: Int, endIndex: Int): String = _content.substring(beginIndex, endIndex)
 
-  /** Insert a string at the index
+  /** Add the inserting event to the historic and call simple insert
     * @param string The string to insert
     * @param index The position of the insertion
     */
   def insert(string: String, index: Int): Unit = {
-  	_historicBefore.prepend(new Insertion(string, index))
-  	_historicBefore.head.redo()
+  	_historicBefore.prepend(new Insertion(this, string, index))
+  	simpleInsert(string, index)
   }
   
-  /** Remove the substring between beginIndex (included) and endIndex(excluded)
+  /** Add the removing event to the historic and call simpleRemove
     * @param beginIndex The beginning index of the deletion
     * @param endIndex The endding index of the deletion
     */
   def remove(beginIndex: Int, endIndex: Int): Unit = {
-  	_historicBefore.prepend(new Deletion(beginIndex, endIndex))
-  	_historicBefore.head.redo()
+  	_historicBefore.prepend(new Deletion(this, beginIndex, endIndex))
+  	simpleRemove(beginIndex, endIndex)
   }
+  
+  /** Insert a string at the index
+    * @param string The string to insert
+    * @param index The position of the insertion
+    */
+  def simpleInsert(string: String, index: Int): Unit = _content = _content.substring(0, index).concat(string).concat(_content.substring(index))
+    
+  /** Remove the substring between beginIndex (included) and endIndex(excluded)
+    * @param beginIndex The beginning index of the deletion
+    * @param endIndex The endding index of the deletion
+    */
+  def simpleRemove(beginIndex: Int, endIndex: Int): Unit = _content = _content.substring(0, beginIndex).concat(_content.substring(endIndex))
   
   /** Undo the last event
     */
@@ -51,36 +63,36 @@ class Buffer(s: String) {
   	_historicBefore.prepend(_historicAfter.remove(0))
   }
   
-  
-  
-  
-  trait Historique {
-	def undo(): Unit
-	def redo(): Unit
-  }
+}
 
-  class Insertion(s: String, i: Int) extends Historique {
-	val _string = s
-	val _index = i
-	
-	def string: String = _string
-	def index: Int = _index
-	
-	def undo(): Unit = new Deletion(_index, _index + _string.length).redo()
-	def redo(): Unit = _content = _content.substring(0, index).concat(string).concat(_content.substring(index))
-  }
- 
-  class Deletion(b: Int, e:Int) extends Historique {
-  	val _beginIndex = b
-  	val _endIndex = e
-  	val _string = _content.substring(_beginIndex, _endIndex)
-  	
-  	def beginIndex: Int = _beginIndex
-  	def endIndex: Int = _endIndex
-  	def string: String = _string
-  	
-  	def undo(): Unit = new Insertion(_string, _beginIndex).redo()
-  	def redo(): Unit = _content = _content.substring(0, beginIndex).concat(_content.substring(endIndex))
-  }
+
+trait Historique {
+  def undo(): Unit
+  def redo(): Unit
+}
+
+class Insertion(b:Buffer, s: String, i: Int) extends Historique {
+  val _string = s
+  val _index = i
+  val _buffer = b
+
+  def string: String = _string
+  def index: Int = _index
   
+  def undo(): Unit = new Deletion(_buffer, _index, _index + _string.length).redo()
+  def redo(): Unit = _buffer.simpleInsert(_string, _index)
+}
+ 
+class Deletion(b: Buffer, begin: Int, end:Int) extends Historique {
+  val _beginIndex = begin
+  val _endIndex = end
+  val _buffer = b
+  val _string = _buffer.select(_beginIndex, _endIndex)
+
+  def beginIndex: Int = _beginIndex
+  def endIndex: Int = _endIndex
+  def string: String = _string
+
+  def undo(): Unit = new Insertion(_buffer, _string, _beginIndex).redo()
+  def redo(): Unit = _buffer.simpleRemove(_beginIndex, _endIndex)
 }
