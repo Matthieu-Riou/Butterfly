1445e75e2935441233fce0be2b156b85d12372bf
Brice Thomas <brice.p.thomas@gmail.com>
2014-11-17 18:05:52 +0100

    Fix styling :)

--- src/test/scala/io/atal/butterfly/BufferTest.scala
+++ src/test/scala/io/atal/butterfly/BufferTest.scala
@@ -22,55 +22,54 @@ class BufferTest extends FlatSpec {
 
     assert(buffer.toString() == buffer.content)
   }
-  
+
   "The Buffer select method" should "return the substring selected" in {
-  	val buffer = new Buffer("Hello world!")  	
-  	  	  	  	
-  	assert(buffer.select(6,11).equals("world"))
+    val buffer = new Buffer("Hello world!")
+
+    assert(buffer.select(6,11).equals("world"))
   }
-  
+
   "The Buffer insert method" should "insert the wanted string in the content at the wanted position" in {
-  	val buffer = new Buffer("Hello world!")
-  	
-  	buffer.insert("king of the ", 6)
-  	
-  	assert(buffer.content == "Hello king of the world!")
+    val buffer = new Buffer("Hello world!")
+
+    buffer.insert("king of the ", 6)
+
+    assert(buffer.content == "Hello king of the world!")
   }
-  
+
   "The Buffer remove method" should "remove the substring defined by positions from the content" in {
-  	val buffer = new Buffer("Hello world!")
-  	
-  	buffer.remove(5,11)
-  	
-  	assert(buffer.content == "Hello!")
+    val buffer = new Buffer("Hello world!")
+
+    buffer.remove(5,11)
+
+    assert(buffer.content == "Hello!")
   }
-  
+
   "The last event" should "undo and redo properly" in {
-  	val buffer = new Buffer("Hello world!")
-  	
-  	buffer.insert("king of the ", 6)
-  	
-  	assert(buffer.content == "Hello king of the world!")
-  	
-  	buffer.undo()
-  	
-  	assert(buffer.content == "Hello world!")
-  	
-  	buffer.redo()
-  	
-  	assert(buffer.content == "Hello king of the world!")
-  	
-  	
-  	buffer.remove(6, 18)
-  	
-  	assert(buffer.content == "Hello world!")
-  	
-  	buffer.undo()
-  	
-  	assert(buffer.content == "Hello king of the world!")
-  	
-  	buffer.redo()
-  	
-  	assert(buffer.content == "Hello world!")
-  } 
+    val buffer = new Buffer("Hello world!")
+
+    buffer.insert("king of the ", 6)
+
+    assert(buffer.content == "Hello king of the world!")
+
+    buffer.undo()
+
+    assert(buffer.content == "Hello world!")
+
+    buffer.redo()
+
+    assert(buffer.content == "Hello king of the world!")
+
+    buffer.remove(6, 18)
+
+    assert(buffer.content == "Hello world!")
+
+    buffer.undo()
+
+    assert(buffer.content == "Hello king of the world!")
+
+    buffer.redo()
+
+    assert(buffer.content == "Hello world!")
+  }
 }
--- src/test/scala/io/atal/butterfly/HistoryTest.scala
+++ src/test/scala/io/atal/butterfly/HistoryTest.scala
@@ -13,109 +13,109 @@ class HistoryTest extends FlatSpec {
 
     assert(history.buffer == buf)
   }
-  
+
   "The History newInsertion and newDeletion methods" should "add the event to the history" in {
-  	val buffer = new Buffer("Hello") 
-  	val history = new History(buffer)
-  	
-  	history._historyBefore should have length 0
-  	
-  	history.newInsertion(" world!", 5) 
-  	buffer.insert(" world!", 5)
-  	
-  	history._historyBefore should have length 1
-  
-  	history.newDeletion(5, 11)
-  	buffer.remove(5, 11)
-  	  	  	  	
+    val buffer = new Buffer("Hello")
+    val history = new History(buffer)
+
+    history._historyBefore should have length 0
+
+    history.newInsertion(" world!", 5)
+    buffer.insert(" world!", 5)
+
+    history._historyBefore should have length 1
+
+    history.newDeletion(5, 11)
+    buffer.remove(5, 11)
+
     history._historyBefore should have length 2
   }
-  
+
   "The History newInsertion and newDeletion methods" should "clear the after history" in {
-  	val buffer = new Buffer("Hello") 
-  	val history = new History(buffer)
-  	
-  	history._historyAfter should have length 0
-  	
-  	history.newInsertion(" world!", 5) 
-  	buffer.insert(" world!", 5)
- 
+    val buffer = new Buffer("Hello")
+    val history = new History(buffer)
+
     history._historyAfter should have length 0
-    
+
+    history.newInsertion(" world!", 5)
+    buffer.insert(" world!", 5)
+
+    history._historyAfter should have length 0
+
     history.undo()
-    
+
     history._historyAfter should have length 1
-    
+
     history.newInsertion(" woooorld!", 5)
     buffer.insert(" woooorld!", 5)
-  	
-  	history._historyAfter should have length 0
-  	
-  	history.undo()
-    
+
+    history._historyAfter should have length 0
+
+    history.undo()
+
     history._historyAfter should have length 1
-    
-    history.newDeletion(2,3)
-    buffer.remove(2,3)
-    
+
+    history.newDeletion(2, 3)
+    buffer.remove(2, 3)
+
     history._historyAfter should have length 0
   }
-  
+
   "The History undo method" should "undo the last event" in {
-  	val buffer = new Buffer("Hello") 
-  	val history = new History(buffer)
-  	
-  	assert(history.buffer.content == "Hello")
-  	
-  	history.newInsertion(" world!", 5) 
-  	buffer.insert(" world!", 5)
-  	
-  	assert(history.buffer.content == "Hello world!")
-    
-    history.undo()
-    
+    val buffer = new Buffer("Hello")
+    val history = new History(buffer)
+
     assert(history.buffer.content == "Hello")
-    
+
+    history.newInsertion(" world!", 5)
+    buffer.insert(" world!", 5)
+
+    assert(history.buffer.content == "Hello world!")
+
+    history.undo()
+
+    assert(history.buffer.content == "Hello")
+
     history.newDeletion(2,3)
     buffer.remove(2,3)
-    
+
     assert(history.buffer.content == "Helo")
-    
+
     history.undo()
-    
+
     assert(history.buffer.content == "Hello")
   }
-  
+
   "The History redo method" should "redo the last undo event" in {
-  	val buffer = new Buffer("Hello") 
-  	val history = new History(buffer)
-  	
-  	assert(history.buffer.content == "Hello")
-  	
-  	history.newInsertion(" world!", 5) 
-  	buffer.insert(" world!", 5)
-  	
-  	assert(history.buffer.content == "Hello world!")
-    
-    history.undo()
-    
+    val buffer = new Buffer("Hello")
+    val history = new History(buffer)
+
     assert(history.buffer.content == "Hello")
-    
+
+    history.newInsertion(" world!", 5)
+    buffer.insert(" world!", 5)
+
+    assert(history.buffer.content == "Hello world!")
+
+    history.undo()
+
+    assert(history.buffer.content == "Hello")
+
     history.redo()
-    
-  	assert(history.buffer.content == "Hello world!")
-    
+
+    assert(history.buffer.content == "Hello world!")
+
     history.newDeletion(2,3)
     buffer.remove(2,3)
-    
+
     assert(history.buffer.content == "Helo world!")
-    
+
     history.undo()
-    
+
     assert(history.buffer.content == "Hello world!")
-    
+
     history.redo()
-    
+
     assert(history.buffer.content == "Helo world!")
   }
 }
